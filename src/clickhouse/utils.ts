11c2224911cbdb840ae54cd8369a867b00a77d34
Castello Govender <castello.govender@jembi.org>
2022-08-23 16:51:04 +0200

    Added quotes for string variables

--- src/clickhouse/utils.ts
+++ src/clickhouse/utils.ts
@@ -9,12 +9,11 @@ const clickhouse = new ClickHouse({
   debug: true,
 });
 
-const insertQuery = (tableName: string, data: object) => `INSERT INTO ${tableName}(${Object.keys(data).join(", ")}) VALUES(${Object.values(data).join(", ")})`;
+const insertQuery = (tableName: string, data: object) => `INSERT INTO ${tableName}(${Object.keys(data).join(", ")}) VALUES('${Object.values(data).join("', '")}')`;
 
 export async function loadDataIntoClickhouse(tableName: string, data: object) {
   try {
-    const query = insertQuery(tableName, data);
-    const response = await clickhouse.insert(query).toPromise();
+    const response = await clickhouse.insert(insertQuery(tableName, data)).toPromise();
     console.log(`Successfully added data into ${tableName} table`, response);
   } catch (error: unknown) {
     console.error(`Couldn't insert data into ${tableName} table`, error);
