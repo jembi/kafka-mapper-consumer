9d77c0ce4b28cc4b0f9d94f2acf8f2c67ea091e4
Castello Govender <castello.govender@jembi.org>
2022-08-23 10:22:18 +0200

    Added schema validation

--- src/util.ts
+++ src/util.ts
@@ -1,9 +1,29 @@
+import Ajv from "ajv";
 import fhirpath from "fhirpath";
 import { Entry, FhirMapping, Table } from "./types";
 
-export const ValidateFhirMappingsJson = (fhirMappings: FhirMapping[]): Boolean => {
-  console.log(fhirMappings);
-  return true;
+const ajv = new Ajv({ allErrors: true, strictTuples: false });
+const schema = require("../schema/fhir-mapping.schema.json");
+delete schema["$schema"];
+const validate = ajv.compile(schema);
+
+const hasUniqueResourceTypes = (fhirMappings: FhirMapping[]) => {
+  const resourceTypes = fhirMappings.map((fhirMapping) => fhirMapping.resourceType);
+  const uniqueResourceTypes = [...new Set(resourceTypes)];
+  const hasUniqueResourceTypes = uniqueResourceTypes.length === fhirMappings.length;
+  return hasUniqueResourceTypes;
+};
+
+export const ValidateFhirMappingsJson = (fhirMappings: FhirMapping[]): Error[] => {
+  let errors: Error[] = [];
+
+  if (!hasUniqueResourceTypes(fhirMappings)) errors.push(new Error("Duplicate resource type "));
+
+  if (!validate(fhirMappings)) {
+    errors = [...errors, ...(validate.errors?.map((error) => new Error(error.message)) ?? [])];
+  }
+
+  return errors;
 };
 
 export const GetTableMappings = (fhirMappings: FhirMapping[], entry: Entry): Table[] => {
