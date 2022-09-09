8c777ec6188c4fcbb1280f917960bf1babb1e88e
Castello Govender <castello.govender@jembi.org>
2022-09-09 09:21:52 +0200

    Updated tests to use the proposed naming convention

--- test/data/mapping/valid-plugin/plugin.ts
+++ test/data/mapping/valid-plugin/plugin.ts
@@ -1,6 +1,6 @@
 export const plugin = (table, entry, tableMapping) => {
-  if (table.rows.patientGivenName && table.rows.patientFamilyName) {
-    table.rows["patientFullName"] = `${table.rows.patientGivenName} ${table.rows.patientFamilyName}`;
+  if (table.rows.patient_given_name && table.rows.patient_family_name) {
+    table.rows["patient_full_name"] = `${table.rows.patient_given_name} ${table.rows.patient_family_name}`;
   }
   return table;
 };
