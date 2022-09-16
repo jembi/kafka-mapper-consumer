2f041ad6cdbce239fa9aa2f32d6933a427ea3f6e
mark-labuschagne <marklabuschagne7@gmail.com>
2022-09-16 16:22:09 +0200

    selectById and alterRow queries

--- src/clickhouse/utils.ts
+++ src/clickhouse/utils.ts
@@ -10,6 +10,14 @@ const clickhouse = new ClickHouse({
   debug: true,
 });
 
+const selectQuery = (table: Table) => `SELECT id FROM ${table.name} WHERE id='${table.rows['id']}'`;
+
+const alterQuery = (table: Table) => `ALTER TABLE ${table.name} UPDATE ${Object.entries(table.rows).map(([key, value]) => `${key}='${value}'`).join(",")} WHERE id='${table.rows['id']}'`;
+
 const insertQuery = (tableName: string, data: object) => `INSERT INTO ${tableName}(${Object.keys(data).join(", ")}) VALUES('${Object.values(data).join("', '")}')`;
+
+export const selectByIdClickhouse = async (table: Table) => clickhouse.query(selectQuery(table)).toPromise();
+
+export const alterRowIntoClickhouse = async (table: Table) => clickhouse.query(alterQuery(table)).toPromise();
 
 export const loadDataIntoClickhouse = async (table: Table) => clickhouse.insert(insertQuery(table.name, table.rows)).toPromise();
