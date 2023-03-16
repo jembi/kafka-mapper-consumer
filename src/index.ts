b62b62a606f3188d5c0b2de7cee5533a0d71fd51
Ryan Crichton <ryan@jembi.org>
2023-03-16 13:55:31 +0200

    Allow registration as a mediator and to use mediator config

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,30 @@
 import { Kafka, logLevel } from "kafkajs";
+import { registerMediator, activateHeartbeat, fetchConfig } from "openhim-mediator-utils"
+import { promisify } from "util"
 import { Entry, FhirMapping, FhirPlugin, Table } from "./types";
 import { GetFhirPlugins, GetTableMappings, ValidateFhirMappingsJson } from "./util";
 import { loadDataIntoClickhouse, selectByIdClickhouse, alterRowIntoClickhouse } from "./clickhouse/utils";
+import mediatorConfig from "./data/mediator-config.json"
 
-const fhirMappings: FhirMapping[] = require("./data/fhir-mapping.json");
-const fhirMappingValidationErrors = ValidateFhirMappingsJson(fhirMappings);
-if (fhirMappingValidationErrors.length > 0) {
-  console.error("Invalid fhir-mapping.json file");
-  fhirMappingValidationErrors.forEach((error) => console.error(error));
-  process.exit(1);
-}
+let fhirMappings: FhirMapping[] = require("./data/fhir-mapping.json");
+// Set default mediator FHIR mapping from config file
+mediatorConfig.config.fhirMappings = JSON.stringify(fhirMappings, null, 2)
 
 const plugins: Map<string, FhirPlugin> = GetFhirPlugins(fhirMappings);
 
-// TODO: find out if http is required for local testing
-const kafkaHost = process.env.KAFKA_HOST || "http://localhost";
-const kafkaPort = process.env.KAFKA_PORT || "9092";
+const kafkaHost = process.env.KAFKA_HOST ?? "localhost";
+const kafkaPort = process.env.KAFKA_PORT ?? "9092";
+const trustSelfSigned = (process.env.TRUST_SELF_SIGNED ?? "false") === "true"
+const openhimAuth = {
+  apiURL: process.env.OPENHIM_API_URL ?? "https://localhost:8080",
+  username: process.env.OPENHIM_USERNAME ?? "root@openhim.org",
+  password: process.env.OPENHIM_PASSWORD ?? "password",
+  trustSelfSigned,
+  urn: "urn:openhim-mediator:kafka-mapper-consumer"
+}
+const shouldRegisterMediator = (process.env.REGISTER_MEDIATOR ?? "false") === "true"
+const fromBeginning = (process.env.KAFKA_FROM_BEGINNING ?? "true") === "true"
+const consumerGroupId = process.env.CONSUMER_GROUP_ID ?? "kafka-mapper-consumer"
 
 const kafka = new Kafka({
   logLevel: logLevel.INFO,
@@ -23,15 +32,44 @@ const kafka = new Kafka({
   clientId: "kafka-mapper-consumer",
 });
 
-const consumer = kafka.consumer({ groupId: "kafka-mapper-consumer" });
-const producer = kafka.producer();
+let consumer = kafka.consumer({ groupId: "kafka-mapper-consumer" });
 
-const run = async () => {
+const run = async (newFhirMappings?) => {
+  if (newFhirMappings) {
+    await consumer.disconnect();
+    // create new consumer so we can subscribe to a (potentially) different set of topics
+    consumer = kafka.consumer({ groupId: consumerGroupId });
+    fhirMappings = newFhirMappings;
+  }
+
+  // Register as an OpenHIM mediator and use that for config management
+  if (shouldRegisterMediator && !newFhirMappings) {
+    console.log("Registering as an OpenHIM Mediator. The OpenHIM will be used to supply latest config not the config file, the config file will provide the default config to the OpenHIM.")
+    await promisify(registerMediator)(openhimAuth, mediatorConfig);
+    const configEmitter = activateHeartbeat(openhimAuth);
+
+    configEmitter.on('config', (config) => {
+      console.log('Received new mapping config, restarting consumer...')
+      // restart consumer with new config
+      run(JSON.parse(config.fhirMappings))
+    })
+
+    const config = await promisify(fetchConfig)(openhimAuth)
+    // set config to that received from the OpenHIM
+    fhirMappings = JSON.parse(config.fhirMappings)
+  }
+
+  const fhirMappingValidationErrors = ValidateFhirMappingsJson(fhirMappings);
+  if (fhirMappingValidationErrors.length > 0) {
+    console.error("Invalid fhir-mapping.json file");
+    fhirMappingValidationErrors.forEach((error) => console.error(error));
+    process.exit(1);
+  }
+
   const topics = fhirMappings.map((fhirMapping) => fhirMapping.resourceType.toLowerCase());
 
   await consumer.connect();
-  await producer.connect();
-  await consumer.subscribe({ topics, fromBeginning: true });
+  await consumer.subscribe({ topics, fromBeginning });
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
@@ -69,7 +107,6 @@ errorTypes.forEach((type) => {
       console.log(`process.on ${type}`);
       console.error(e);
       await consumer.disconnect();
-      await producer.disconnect();
       process.exit(0);
     } catch (_) {
       process.exit(1);
@@ -81,7 +118,6 @@ signalTraps.forEach((type) => {
   process.once(type, async () => {
     try {
       await consumer.disconnect();
-      await producer.disconnect();
     } finally {
       process.kill(process.pid, type);
     }
