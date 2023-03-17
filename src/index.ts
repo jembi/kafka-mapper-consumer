01db545a6d349bb7f31ee914791b5983250b498c
michaelloosen <thorne199@gmail.com>
2023-03-17 12:27:37 +0200

    Update brokerstring Clean code PLAT-671

--- src/index.ts
+++ src/index.ts
@@ -17,10 +17,7 @@ const plugins: Map<string, FhirPlugin> = GetFhirPlugins(fhirMappings);
 const kafkaHosts = process.env.KAFKA_HOST || "kafka-01";
 const kafkaPort = process.env.KAFKA_PORT || "9092";
 
-let brokersString: string[] = [];
-kafkaHosts.split(",").forEach((host) => {
-  brokersString.push(`${host}:${kafkaPort}`);
-});
+const brokersString: string[] = kafkaHosts.split(',').map(host => `${host}:${kafkaPort}`);
 
 const kafka = new Kafka({
   logLevel: logLevel.INFO,
