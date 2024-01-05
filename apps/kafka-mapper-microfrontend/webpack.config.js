0b1db611b4440e8a464529c37ce9b38005344b56
Mohamed Chedli <chedli@symbionix.co>
2024-01-05 02:02:18 +0100

    TB-328 Fix "Can't resolve 'react/jsx-runtime" error caused by React 17

--- apps/kafka-mapper-microfrontend/webpack.config.js
+++ apps/kafka-mapper-microfrontend/webpack.config.js
@@ -11,5 +11,12 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    resolve: {
+      // Fix "Can't resolve 'react/jsx-runtime" error caused by React 17 not having the correct exports entries
+      fallback: {
+        "react/jsx-runtime": "react/jsx-runtime.js",
+        "react/jsx-dev-runtime": "react/jsx-dev-runtime.js",
+      },
+    },
   });
 };
