7b4ab614bec2622df3876bb2a865e91d0513b645
Mohamed Chedli <chedli@symbionix.co>
2024-01-15 18:37:06 +0100

    TB-362 Add FHIR Bundle and FHIR Resource object validation

--- apps/kafka-mapper-microfrontend/src/components/FhirResourcesLoader/FhirResourcesLoader.tsx
+++ apps/kafka-mapper-microfrontend/src/components/FhirResourcesLoader/FhirResourcesLoader.tsx
@@ -15,6 +15,61 @@ import { Bundle } from "fhir/r4";
 
 export function FhirResourcesLoader() {
   const { activeFhirResource, setFhirBundle } = useFhirMapperConfig();
+
+  function isValidBundle(objectToTest: any): boolean {
+    try {
+      // Attempt to cast the object to the Bundle type
+      const bundle = objectToTest as Bundle;
+
+      // Check for required properties and types
+      if (
+        !bundle.resourceType ||
+        bundle.resourceType !== "Bundle" ||
+        // According to the FHIR specification, the id property within a FHIR Bundle is not strictly mandatory for the Bundle to be considered valid.
+        // However, it's highly recommended to include it.
+        // !bundle.id ||
+        !bundle.type ||
+        !Array.isArray(bundle.entry)
+      ) {
+        return false;
+      }
+
+      // Perform additional validation checks as needed,
+      // e.g., checking entry types or specific resource constraints
+
+      return true;
+    } catch (error) {
+      // Handle casting errors, indicating invalid Bundle structure
+      return false;
+    }
+  }
+
+  function isValidResource(objectToTest: any): boolean {
+    try {
+      // Attempt to cast the object to the FhirResource type
+      const resource = objectToTest as FhirResource;
+
+      // Check for required properties and types
+      if (
+        !resource.resourceType ||
+        resource.resourceType == "Bundle"
+        //!resource.id
+        //!resource.meta ||
+        //!resource.meta.versionId
+      ) {
+        return false;
+      }
+
+      // Perform additional validation checks as needed,
+      // e.g., checking specific resource constraints
+
+      return true;
+    } catch (error) {
+      // Handle casting errors, indicating invalid FHIR Resource structure
+      return false;
+    }
+  }
+
   const readJsonFile = (file: Blob) =>
     new Promise((resolve, reject) => {
       const fileReader = new FileReader();
@@ -31,9 +86,32 @@ export function FhirResourcesLoader() {
 
   const handleFormSubmit = async (file) => {
     if (file) {
-      const parsedData = await readJsonFile(file);
-      const bundle = parsedData as Bundle<FhirResource>;
-      setFhirBundle(bundle);
+      try {
+        const parsedData = await readJsonFile(file);
+        if (isValidBundle(parsedData)) {
+          const fhirBundle = parsedData as Bundle<FhirResource>;
+          setFhirBundle(fhirBundle);
+        } else if (isValidResource(parsedData)) {
+          const resource = parsedData as FhirResource;
+          const fhirBundle = {
+            resourceType: "Bundle",
+            type: "collection",
+            entry: [
+              {
+                resource: resource,
+              },
+            ],
+          } as Bundle<FhirResource>;
+          console.log("FHIR Resource converted to Bundle: ", fhirBundle);
+          setFhirBundle(fhirBundle as Bundle<FhirResource>);
+        } else {
+          console.log("Invalid FHIR Resource or Bundle");
+          window.alert("Invalid FHIR Resource or Bundle");
+        }
+      } catch (error) {
+        console.error("An error occurred while processing the file:", error);
+        window.alert("An error occurred while processing the file.");
+      }
     }
   };
 
